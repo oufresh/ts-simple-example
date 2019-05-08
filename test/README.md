b2d71cc67cfe090c66952be79eb17a756fc972ed
oufresh <sergio.gennari@gmail.com>
2019-05-08 18:20:13 +0200

    test project

---
+++ test/README.md
@@ -0,0 +1,8 @@
+# test
+
+Simple project to test Typecript modules.
+
+- Modules are built with rollup
+- Test is bundled with Parcel
+
+:)
---
+++ test/index.ts
@@ -0,0 +1,5 @@
+import * as ou from "oufresh-module";
+
+console.log("Prova import del modulo");
+const str = ou.testMethod("str");
+console.log(str);
