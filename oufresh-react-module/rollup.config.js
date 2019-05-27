b7160135ffe107694ada5ff6d28ddb8368846684
oufresh <sergio.gennari@gmail.com>
2019-05-27 14:05:37 +0200

    added build, still missing demo

--- oufresh-react-module/rollup.config.js
+++ oufresh-react-module/rollup.config.js
@@ -1,12 +1,15 @@
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import svgr from '@svgr/rollup'
 
 import pkg from "./package.json";
 
 export default {
-  input: "src/index.ts",
+  input: "src/index.tsx",
   output: [
     {
       file: pkg.main,
@@ -23,6 +26,11 @@ export default {
   ],
   plugins: [
     external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
