f31a97799f1eb4e4450e3e62793de7565c3b6fa1
oufresh <sergio.gennari@gmail.com>
2019-05-29 18:00:24 +0200

    test on css modules

--- oufresh-react-module/src/MyComponent.tsx
+++ oufresh-react-module/src/MyComponent.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import * as style "./MyComponent.module.css";
 
 export interface IMyComponentProps {
     text: string;
-};
+}
 
 export const MyComponent = (props: IMyComponentProps) => {
     const str = "Hello, the text is: " + props.text;
-    return <div><label>{str}</label></div>;
+    return <div className={"MyComponentBackGround"}><label>{str}</label></div>;
 };
