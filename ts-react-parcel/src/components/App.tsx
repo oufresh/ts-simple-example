992efe03f2ab229d31cd153a302c119e948d3dd1
oufresh <sergio.gennari@gmail.com>
2020-01-06 22:26:28 +0100

    ts parcel

---
+++ ts-react-parcel/src/components/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as style from "./App.module.css";
+
+export const App: React.SFC<{}> = () => (
+  <div className={style.container}>
+    <h3>Hello!</h3>
+  </div>
+);
