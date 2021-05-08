9a8b5a1ad206b614e505059882e05f9d2f75710c
oufresh <sergio.gennari@gmail.com>
2021-05-08 16:35:14 +0200

    Delete test directory

--- test/README.md
+++
@@ -1,8 +0,0 @@
-# test
-
-Simple project to test Typecript modules.
-
-- Modules are built with rollup
-- Test is bundled with Parcel
-
-:)
--- test/index.ts
+++
@@ -1,5 +0,0 @@
-import * as ou from "oufresh-module";
-
-console.log("Prova import del modulo");
-const str = ou.testMethod("str");
-console.log(str);
